505e4553cbd07e4bdedfd1e45528ad2da4ec7523
jheart-vic <adebowalevictorjheart@gmail.com>
2022-08-18 18:51:27 +0100

    Modified class

--- class.rb
+++ class.rb
@@ -6,6 +6,10 @@ class Mylist
   def initialize(*list)
     @list = list
   end
+
+  def each(&block)
+    @list.each(&block)
+  end
 end
 
 # Create instance of MyList
@@ -15,11 +19,13 @@ list_test = Mylist.new(1, 2, 3, 4)
 # Test all? method
 
 list_test.all? { |e| e < 5 } # => true
+list_test.all? { |e| e > 5 } # => false
 
 # Test any? method
 
 list_test.any? { |e| e == 2 } # => true
 list_test.any? { |e| e == 5 } # => false
 
-# Test filter method
-list_test.filter { |e| e.even }
+# Test filter? method
+
+list_test.filter? { |e| e.even? }
